2aa259579a5aa501aad6c99cc298dc234256cc2c
AnthonyDolly <anthonyluiggydollyO@gmail.com>
2025-03-01 18:13:08 -0500

    Add GroupsModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { BombosModule } from './bombos/bombos.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { envs } from './config/envs';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(envs.mongodbUri), BombosModule],
+  imports: [
+    MongooseModule.forRoot(envs.mongodbUri),
+    BombosModule,
+    GroupsModule,
+  ],
   controllers: [],
   providers: [],
 })
