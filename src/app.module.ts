899f222c8040d552923af1e154629efe05306c0a
AnthonyDolly <anthonyluiggydollyO@gmail.com>
2025-03-01 18:45:07 -0500

    Add TournamentsModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { BombosModule } from './bombos/bombos.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { envs } from './config/envs';
 import { GroupsModule } from './groups/groups.module';
+import { TournamentsModule } from './tournaments/tournaments.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(envs.mongodbUri),
     BombosModule,
     GroupsModule,
+    TournamentsModule,
   ],
   controllers: [],
   providers: [],
