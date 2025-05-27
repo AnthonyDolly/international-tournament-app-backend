b0cc4009a39f41ccdae18ea0aad3244adfa84b0d
AnthonyDolly <anthonyluiggydollyO@gmail.com>
2025-05-26 21:19:11 -0500

    feat: add draw knockout and qualified teams endpoints to group classification controller

--- src/group-classification/group-classification.controller.ts
+++ src/group-classification/group-classification.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Query, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Param } from '@nestjs/common';
 import { GroupClassificationService } from './group-classification.service';
 import { CreateGroupClassificationsDto } from './dto/create-group-classification.dto';
 import { QueryGroupDto } from './dto/query-group.dto';
+import { DrawKnockoutDto } from './dto/draw-knockout.dto';
 
 @Controller('group-classification')
 export class GroupClassificationController {
@@ -22,5 +23,17 @@ export class GroupClassificationController {
   @Post()
   create(@Body() createGroupClassificationDto: CreateGroupClassificationsDto) {
     return this.groupClassificationService.create(createGroupClassificationDto);
+  }
+
+  @Post('draw-knockout')
+  drawKnockout(@Body() drawKnockoutDto: DrawKnockoutDto) {
+    return this.groupClassificationService.drawKnockoutMatchups(
+      drawKnockoutDto.tournamentId,
+    );
+  }
+
+  @Get('qualified-teams/:tournamentId')
+  getQualifiedTeams(@Param('tournamentId') tournamentId: string) {
+    return this.groupClassificationService.getQualifiedTeams(tournamentId);
   }
 }
