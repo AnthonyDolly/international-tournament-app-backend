6f2da07ad2de88ea94e4412cd7a8907133d732e8
AnthonyDolly <anthonyluiggydollyO@gmail.com>
2025-06-08 03:41:10 -0500

    refactor: remove unused 'bombo' property from TeamResponse and simplify PopulatedTeamResponse interface

--- src/teams/types/team.types.ts
+++ src/teams/types/team.types.ts
@@ -1,20 +1,14 @@
 import { Types } from 'mongoose';
-import { SouthAmericanCountry, BomboType } from '../constants/team.constants';
+import { SouthAmericanCountry } from '../constants/team.constants';
 
 export interface TeamResponse {
   _id: Types.ObjectId;
   name: string;
   country: SouthAmericanCountry;
-  bombo: BomboType;
   logo: string | null;
   isParticipating: boolean;
   isCurrentChampion: boolean;
   isFromQualifyingStage: boolean;
 }
 
-export interface PopulatedTeamResponse extends Omit<TeamResponse, 'bombo'> {
-  bombo: {
-    _id: Types.ObjectId;
-    name: string;
-  };
-}
+export interface PopulatedTeamResponse extends TeamResponse {}
